9b0d10d3805d3538002faf0f56b2407146228f1b
Matrixnexus <matrixnexuscoretech@gmail.com>
2026-01-09 15:43:50 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import Pricing from "./pages/Pricing";
 import Checkout from "./pages/Checkout";
 import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
-import AuthCallback from '@/pages/AuthCallback';
 
 const queryClient = new QueryClient();
 
@@ -30,7 +29,6 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
-            <Route path="/auth/callback" element={<AuthCallback />} /> {/* ADD THIS LINE */}
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/jobs" element={<Jobs />} />
             <Route path="/jobs/:id" element={<JobDetail />} />
